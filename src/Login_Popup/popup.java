a9f2eac866abefb358fcab6041b344642317ebff
Rutvik <rutvikpatel.rm@gmail.com>
2019-03-28 16:24:43 -0700

    updating the code

--- src/Login_Popup/popup.java
+++ src/Login_Popup/popup.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class popup {
 
@@ -14,12 +15,18 @@ public class popup {
 		System.setProperty("webdriver.chrome.driver","chromedriver");
 		WebDriver driver = new ChromeDriver();
         driver.get("https://mdbootstrap.com/docs/jquery/modals/forms/");
-       // driver.findElement(By.xpath("//a[text()='Launch Modal Login Form']")).click();
+        
+      
+    
+        Thread.sleep(5000);
+       
+        
+       // driver.findElement(By.xpath("")).click();
         Thread.sleep(3000);
         driver.findElement(By.id("navbar-static-login")).click();
         Thread.sleep(2000);
-        driver.findElement(By.xpath("//*[@class=\"form-control\"]")).sendKeys("rutvikaems@gmail.com");
-        //Thread.sleep(3000);
+        driver.findElement(By.xpath("//*[@class=\"form-control\"]")).sendKeys("abc123@gmail.com");
+        Thread.sleep(3000);
         //driver.findElement(By.xpath("//*[@class=\"form-control\"]")).sendKeys("rutvik");
         Thread.sleep(2000);
         driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("sacstate500");
@@ -29,18 +36,25 @@ public class popup {
         Thread.sleep(2000);
         
         driver.findElement(By.xpath("//*[@id=\"navbarLogin\"]/div/div/div[1]/ul/li[2]/a")).click();
-        Thread.sleep(2000);
+      Thread.sleep(2000);
         driver.findElement(By.xpath("//*[@id=\"signonname\"]")).sendKeys("Rutvik");
         Thread.sleep(2000);
-        driver.findElement(By.xpath("//*[@id=\"signonusername\"]")).sendKeys("rutvik95");
+        driver.findElement(By.xpath("//*[@id=\"signonusername\"]")).sendKeys("abc12457");
         Thread.sleep(2000);
-        driver.findElement(By.xpath("//*[@id=\"email\"]")).sendKeys("rutvikaems@gmail.com");
+        driver.findElement(By.xpath("//*[@id=\"email\"]")).sendKeys("abc123@gmail.com");
         Thread.sleep(2000);
         driver.findElement(By.xpath("//*[@id=\"signonpassword\"]")).sendKeys("sacstate500");
         Thread.sleep(2000);
         driver.findElement(By.xpath("//*[@id=\"password2\"]")).sendKeys("sacstate500");
         Thread.sleep(2000);
         driver.findElement(By.xpath("//*[@id=\"AJAXAuthRegisterBtn\"]")).click();
+	
+//        Thread.sleep(2000);
+//        driver.findElement(By.xpath(" //*[@id=\"AJAXAuthRegisterBtn\"]")).click();
+//        Thread.sleep(2000);
+        Thread.sleep(5000);
+        driver.findElement(By.id("navbar-account")).click();
+        //Thread.sleep(2000);
 	}
 
 }
